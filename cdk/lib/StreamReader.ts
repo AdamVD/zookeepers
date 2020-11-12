898c5485ae135baba774b4db9f4048452ad30a48
Adam Del Rosso <avdonle@outlook.com>
2020-11-12 16:20:35 -0500

    Add ECS service with stream reader

--- cdk/lib/StreamReader.ts
+++ cdk/lib/StreamReader.ts
@@ -1,4 +1,7 @@
 import * as cdk from '@aws-cdk/core';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 import {DockerImageAsset} from "@aws-cdk/aws-ecr-assets";
 import * as path from "path";
 
@@ -8,6 +11,19 @@ export class StreamReader extends cdk.Construct {
 
     const dockerImage = new DockerImageAsset(this, 'StreamReaderImage', {
       directory: path.join(__dirname, '..', '..', 'stream_reader')
+    });
+
+    const vpc = new ec2.Vpc(this, 'StreamReaderVPC');  // default VPC
+
+    const cluster = new ecs.Cluster(this, 'StreamReaderCluster', {
+      vpc: vpc
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'StreamReaderFargateService', {
+      cluster: cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromDockerImageAsset(dockerImage)
+      }
     });
 
     new cdk.CfnOutput(this, 'ImageURI', {
