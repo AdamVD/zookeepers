684fb8836ecf86d876dd2f1a240d623e65acdc1a
Adam Del Rosso <avdonle@outlook.com>
2020-11-12 15:52:07 -0500

    Add docker image as CDK asset
    Uploads to ECR

--- cdk/lib/StreamReader.ts
+++ cdk/lib/StreamReader.ts
@@ -1,10 +1,17 @@
 import * as cdk from '@aws-cdk/core';
-import * as ecr from '@aws-cdk/aws-ecr';
+import {DockerImageAsset} from "@aws-cdk/aws-ecr-assets";
+import * as path from "path";
 
 export class StreamReader extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string) {
     super(scope, id);
 
-    const ecrRepository = new ecr.Repository(this,'Repository');
+    const dockerImage = new DockerImageAsset(this, 'StreamReaderImage', {
+      directory: path.join(__dirname, '..', '..', 'stream_reader')
+    });
+
+    new cdk.CfnOutput(this, 'ImageURI', {
+      value: dockerImage.imageUri
+    })
   }
 }
