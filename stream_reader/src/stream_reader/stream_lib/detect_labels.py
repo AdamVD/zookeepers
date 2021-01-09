9f46771b1920014409cf0fc1a3f7f17258d16a46
Adam Del Rosso <avdonle@outlook.com>
2021-01-09 11:50:18 -0500

    Remove STS calls - switch back to same-account usage

--- stream_reader/src/stream_reader/stream_lib/detect_labels.py
+++ stream_reader/src/stream_reader/stream_lib/detect_labels.py
@@ -6,24 +6,10 @@ import enum
 from botocore.exceptions import ClientError
 from typing import Tuple
 
-# cross-account rekognition access
-sts = boto3.client('sts')
-assumed_role = sts.assume_role(
-    RoleArn=os.environ['REKOGNITION_ROLE_ARN'],
-    RoleSessionName='RekognitionAssumedSession'
-)
-logging.info(f'Assumed cross-account role for rekognition access')
-credentials = assumed_role['Credentials']
-
-rekognition = boto3.client(
-    'rekognition',
-    aws_access_key_id=credentials['AccessKeyId'],
-    aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['SessionToken']
-)
+rekognition = boto3.client('rekognition', region_name=os.environ['REGION'])
 logging.info('Created rekognition boto3 client')
 
-PROJECT_VERSION_ARN = 'arn:aws:rekognition:us-east-1:591083098024:project/zookeepers_polarbear/version/zookeepers_polarbear.2020-11-15T22.20.57/1605496857456'
+PROJECT_VERSION_ARN = os.environ['REKOGNITION_MODEL_ARN']
 started_project_version = False
 
 
